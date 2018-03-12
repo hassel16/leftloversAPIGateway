4033caadbd7de57135fa271a15536de075d8bf42
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-12 11:09:39 +0100

    bug

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.all('/:needServiceName/*', function (req, res) {
         res.status(400).json(new Error('Der angeforderte Service exitiert aktuell unter diesem Namen nicht'));
     }
     apiProxy.web(req, res,
-        { target: `https://standortservice.herokuapp.com/`,agent  : https.globalAgent,
+        { target: `standortservice.herokuapp.com:443/`,agent  : https.globalAgent,
     proxyTimeout:600000},
         function (e,ereq,eres,url) {
             res.status(502).json(new Error(`${e.message} Timeout ${req.params.needServiceName} Fehler beim Anfordern der Ressourcen`));
