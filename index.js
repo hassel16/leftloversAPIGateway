a9320294f0ee4fdbd0cafbc200e4e527b3132c57
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-21 01:49:11 +0100

    upodate methode

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));
     next();
 });
 
