76e8d73734da670ad84281bcd8e2beca6c53defa
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-29 12:17:25 +0200

    s

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const routerObj = Router.readFromJSON();
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers","Authorization");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
     next();
